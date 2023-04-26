4c3d89f9fb1ff6d54a088bf5d972d7813b773b21
Barissa-Imran <imranbarissa@gmail.com>
2023-04-27 01:20:44 +0300

    Add task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Install nginx webserver
+package { 'nginx':
+    ensure  => installed,
+}
+
+file_line { 'aaaaa':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'listen 80 default_server;',
+    line   => 'rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+    content => 'Hello World!',
+}
+
+service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+}
