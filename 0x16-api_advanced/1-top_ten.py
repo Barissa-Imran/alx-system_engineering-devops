a055d7f353cfe00e5ec76198a51097f25d0676b1
Barissa-Imran <imranbarissa@gmail.com>
2023-06-06 22:33:45 +0300

    add task 1: Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" prints the titles of the first 10 hot posts"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 \
+            Mobile Safari/537.36"
+    }
+    param = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=param, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(top.get("data").get("title")) for top in results.get("children")]
