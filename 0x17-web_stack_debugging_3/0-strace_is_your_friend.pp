4bbe785d9a8a9a139b1045cf98caa94ad280deaa
Barissa-Imran <imranbarissa@gmail.com>
2023-06-08 00:05:51 +0300

    fix task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Solve 500 error on apache
 
 exec { 'fix':
- command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
- path    => '/usr/local/bin/:/bin/'
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/local/bin/:/bin/'
 }
